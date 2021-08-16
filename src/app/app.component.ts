9ab4a12e88c2621c79e1e424795979f3499b79ae
Charles Witherspoon <charles.v.witherspoon@gmail.com>
2021-08-15 23:11:25 -0400

    Little bit of refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,274 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task';
+import { Event } from './models/event';
 
 
-export interface Event {
-  title: string;
-  startDate: Date;
-  duration: number;
-  color: string;
-  width: number;
-  offset: number;
-}
+const MOCK_TASKS = [
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+  {
+    title: "Walk dog",
+    isComplete: false
+  },
+  {
+    title: "Get oil change",
+    isComplete: false
+  },
+  {
+    title: "Groceries",
+    isComplete: false
+  },
+  {
+    title: "Finish project 1",
+    isComplete: false
+  },
+];
+
+const MOCK_HOURS = [
+  {
+    date: new Date('2021-08-15T00:00:00'),
+    events: [
+      {
+        title: "Sleep",
+        startDate: new Date('2021-08-15T00:00:00'),
+        duration: 360,
+        color: "blue",
+        width: 50,
+        offset: 0
+      }
+    ]
+  },
+  {
+    date: new Date('2021-08-15T01:00:00'),
+    events: [
+      {
+        title: "Sleep Interruption",
+        startDate: new Date('2021-08-15T01:00:00'),
+        duration: 120,
+        color: "red",
+        width: 50,
+        offset: 50
+      }
+    ]
+  },
+  {
+    date: new Date('2021-08-15T02:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T03:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T04:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T05:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T06:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T07:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T08:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T09:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T10:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T11:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T12:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T13:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T14:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T15:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T16:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T17:00:00'),
+    events: [
+      {
+        title: "Gym",
+        startDate: new Date('2021-08-15T17:00:00'),
+        duration: 90,
+        color: "green",
+        width: 100,
+        offset: 0
+      }
+    ]
+  },
+  {
+    date: new Date('2021-08-15T18:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T19:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T20:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T21:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T22:00:00'),
+    events: [
+
+    ]
+  },
+  {
+    date: new Date('2021-08-15T23:00:00'),
+    events: [
+
+    ]
+  },
+];
 
 export interface Hour {
   date: Date;
@@ -23,273 +282,8 @@ export interface Hour {
 })
 export class AppComponent {
   title = 'smart-planner';
-  tasks: Task[] = [
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-    {
-      title: "Walk dog",
-      isComplete: false
-    },
-    {
-      title: "Get oil change",
-      isComplete: false
-    },
-    {
-      title: "Groceries",
-      isComplete: false
-    },
-    {
-      title: "Finish project 1",
-      isComplete: false
-    },
-  ];
-
-
-  hours: Hour[] = [
-    {
-      date: new Date('2021-08-15T00:00:00'),
-      events: [
-        {
-          title: "Sleep",
-          startDate: new Date('2021-08-15T00:00:00'),
-          duration: 360,
-          color: "blue",
-          width: 50,
-          offset: 0
-        }
-      ]
-    },
-    {
-      date: new Date('2021-08-15T01:00:00'),
-      events: [
-        {
-          title: "Sleep Interruption",
-          startDate: new Date('2021-08-15T01:00:00'),
-          duration: 120,
-          color: "red",
-          width: 50,
-          offset: 50
-        }
-      ]
-    },
-    {
-      date: new Date('2021-08-15T02:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T03:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T04:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T05:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T06:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T07:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T08:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T09:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T10:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T11:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T12:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T13:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T14:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T15:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T16:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T17:00:00'),
-      events: [
-        {
-          title: "Gym",
-          startDate: new Date('2021-08-15T17:00:00'),
-          duration: 90,
-          color: "green",
-          width: 100,
-          offset: 0
-        }
-      ]
-    },
-    {
-      date: new Date('2021-08-15T18:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T19:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T20:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T21:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T22:00:00'),
-      events: [
-
-      ]
-    },
-    {
-      date: new Date('2021-08-15T23:00:00'),
-      events: [
-
-      ]
-    },
-  ];
+  tasks: Task[] = MOCK_TASKS;
+  hours: Hour[] = MOCK_HOURS;
 
   // calculateEventWidth(event: Event): number {
   //   // get number events that would overlap
---
+++ src/app/models/event.ts
@@ -0,0 +1,8 @@
+export interface Event {
+    title: string;
+    startDate: Date;
+    duration: number;
+    color: string;
+    width: number;
+    offset: number;
+}
