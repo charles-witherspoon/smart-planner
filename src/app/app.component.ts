8bfbfd80e70342c5665755be08efe2db065c7f2f
Charles Witherspoon <charles.v.witherspoon@gmail.com>
2021-08-15 23:05:32 -0400

    Added/styled events (hardcoded)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task';
 
+
+export interface Event {
+  title: string;
+  startDate: Date;
+  duration: number;
+  color: string;
+  width: number;
+  offset: number;
+}
+
+export interface Hour {
+  date: Date;
+  events: Event[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -106,31 +121,198 @@ export class AppComponent {
       isComplete: false
     },
   ];
-  hours: Date[] = [
-    new Date('2021-08-15T00:00:00'),
-    new Date('2021-08-15T01:00:00'),
-    new Date('2021-08-15T02:00:00'),
-    new Date('2021-08-15T03:00:00'),
-    new Date('2021-08-15T04:00:00'),
-    new Date('2021-08-15T05:00:00'),
-    new Date('2021-08-15T06:00:00'),
-    new Date('2021-08-15T07:00:00'),
-    new Date('2021-08-15T08:00:00'),
-    new Date('2021-08-15T09:00:00'),
-    new Date('2021-08-15T10:00:00'),
-    new Date('2021-08-15T11:00:00'),
-    new Date('2021-08-15T12:00:00'),
-    new Date('2021-08-15T13:00:00'),
-    new Date('2021-08-15T14:00:00'),
-    new Date('2021-08-15T15:00:00'),
-    new Date('2021-08-15T16:00:00'),
-    new Date('2021-08-15T17:00:00'),
-    new Date('2021-08-15T18:00:00'),
-    new Date('2021-08-15T19:00:00'),
-    new Date('2021-08-15T20:00:00'),
-    new Date('2021-08-15T21:00:00'),
-    new Date('2021-08-15T22:00:00'),
-    new Date('2021-08-15T23:00:00'),
+
+
+  hours: Hour[] = [
+    {
+      date: new Date('2021-08-15T00:00:00'),
+      events: [
+        {
+          title: "Sleep",
+          startDate: new Date('2021-08-15T00:00:00'),
+          duration: 360,
+          color: "blue",
+          width: 50,
+          offset: 0
+        }
+      ]
+    },
+    {
+      date: new Date('2021-08-15T01:00:00'),
+      events: [
+        {
+          title: "Sleep Interruption",
+          startDate: new Date('2021-08-15T01:00:00'),
+          duration: 120,
+          color: "red",
+          width: 50,
+          offset: 50
+        }
+      ]
+    },
+    {
+      date: new Date('2021-08-15T02:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T03:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T04:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T05:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T06:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T07:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T08:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T09:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T10:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T11:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T12:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T13:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T14:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T15:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T16:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T17:00:00'),
+      events: [
+        {
+          title: "Gym",
+          startDate: new Date('2021-08-15T17:00:00'),
+          duration: 90,
+          color: "green",
+          width: 100,
+          offset: 0
+        }
+      ]
+    },
+    {
+      date: new Date('2021-08-15T18:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T19:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T20:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T21:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T22:00:00'),
+      events: [
+
+      ]
+    },
+    {
+      date: new Date('2021-08-15T23:00:00'),
+      events: [
+
+      ]
+    },
   ];
+
+  // calculateEventWidth(event: Event): number {
+  //   // get number events that would overlap
+  //   const flatMap = (f: Function, arr: any[]) => arr.reduce((x, y) => [...x, ...f(y)], [])
+  //   const allEvents: Event[] = flatMap([].concat, this.hours.map(hour => hour.events));
+  //   const overlappingEvents: Event[] = allEvents.filter(e => this.eventsOverlap(event, e));
+
+  //   // divide 100 by that number
+  //   return overlappingEvents.length > 0 ?
+  //     100 / overlappingEvents.length
+  //     : 100;
+  // }
+
+  // eventsOverlap(main: Event, test: Event): boolean {
+  //   const mainEnd: Date = main.startDate;
+  //   mainEnd.setUTCHours(main.startDate.getHours() + (main.duration / 60));
+
+  //   const testEnd: Date = test.startDate;
+  //   testEnd.setUTCHours(test.startDate.getHours() + (test.duration / 60));
+
+
+  //   return (test.startDate >= main.startDate && test.startDate <= mainEnd)
+  //     || (main.startDate >= test.startDate && main.startDate <= testEnd);
+  // }
 }
 
