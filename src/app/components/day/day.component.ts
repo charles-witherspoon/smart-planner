e85a9496cc0f91af732ceaa6cd583cc11a7c666a
Charles Witherspoon <charles.v.witherspoon@gmail.com>
2021-08-20 02:17:39 -0400

    changed theme

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { LogType } from 'src/app/models/log-type';
 import { LogService } from 'src/app/services/log.service';
 import { EventAddDialogComponent } from '../dialog/event-add-dialog/event-add-dialog.component';
@@ -11,9 +12,22 @@ import { EventAddDialogComponent } from '../dialog/event-add-dialog/event-add-di
 })
 export class DayComponent implements OnInit {
 
-  constructor(private logger: LogService, private dialog: MatDialog) { }
+  //#region Public Properties
+
+  /**
+   * The current page
+   */
+  public route: string;
+  //#endregion
+
+  constructor(private logger: LogService, private dialog: MatDialog, private router: Router) {
+    this.route = '';
+
+  }
 
   ngOnInit(): void {
+    this.route = this.router.url;
+    console.log(this.route)
   }
 
 
@@ -28,5 +42,18 @@ export class DayComponent implements OnInit {
     this.dialog.open(EventAddDialogComponent);
   }
 
+  public goToCalendar(): void {
+    this.router.navigate(['']);
+  }
+
+  public goToDay(): void {
+    if (this.route === '/day') {
+      this.router.navigate(['']);
+    } else {
+
+      this.router.navigate(['/day']);
+    }
+
+  }
   //#endregion
 }
